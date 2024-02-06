3af55e5b0e6998a5fa32452a938fd8762e844d49
Omar Sh. Dieh <omarsheikhdieh@gmail.com>
2024-02-06 10:11:06 +0100

    lab solution

---
+++ src/pages/ProductListPage.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import ProductCard from "../components/ProductCard";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function ProductListPage() {
+  const [products, setProducts] = useState([]);
+
+  // frontend route : /product/details/15
+  // backend route : /products/15
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        const products = response.data;
+        setProducts(products);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="px-4 py-8 border-b w-full">
+      {products &&
+        products.map((product) => {
+          return (
+            <Link to={`/product/details/${product.id}`} key={product.id}>
+              <ProductCard {...product} />
+            </Link>
+          );
+        })}
+    </div>
+  );
+}
+
+export default ProductListPage;
